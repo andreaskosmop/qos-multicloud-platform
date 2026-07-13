69c7e7b839a3f6844b3f025999f5890f94a47922
andreaskosmop <andreaskosmop@gmail.com>
2026-07-13 19:38:10 +0300

    Update V1__create_users_table.sql

--- backend/src/main/resources/db/migration/V1__create_users_table.sql
+++ backend/src/main/resources/db/migration/V1__create_users_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (
-    id                   UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    id                   VARCHAR(255) PRIMARY KEY,
     username             VARCHAR(50)  UNIQUE NOT NULL,
     full_name            VARCHAR(100) NOT NULL,
     email                VARCHAR(255) UNIQUE NOT NULL,
